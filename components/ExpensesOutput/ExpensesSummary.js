8062fa540302f854ee50ea1945fd0a6043a4bd3e
Timilehin Arigbede <Harriemannie@gmail.com>
2022-06-14 12:13:17 +0100

    Creating Key App Components to Display Expenses

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { Text, View, StyleSheet } from "react-native";
+
+const ExpensesSummary = () => {
+  return (
+    <View>
+      <Text>Last 7 Days</Text>
+      <Text>$177.85</Text>
+    </View>
+  );
+};
+
+export default ExpensesSummary;
