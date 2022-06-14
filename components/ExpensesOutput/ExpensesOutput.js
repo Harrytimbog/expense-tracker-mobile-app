288d81dd720d844673f906f1271f4c6c54c897f2
Timilehin Arigbede <Harriemannie@gmail.com>
2022-06-14 14:18:06 +0100

    Working on Expense List Items

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -34,6 +34,30 @@ const DUMMY_EXPENSES = [
     amount: 19.79,
     date: new Date("2022-05-1"),
   },
+  {
+    id: "e6",
+    description: "Go To Club",
+    amount: 43.29,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e7",
+    description: "Some Doughnuts",
+    amount: 5.99,
+    date: new Date("2022-06-13"),
+  },
+  {
+    id: "e8",
+    description: "Get A Book",
+    amount: 19.79,
+    date: new Date("2022-03-1"),
+  },
+  {
+    id: "e9",
+    description: "Buy A Charger",
+    amount: 19.79,
+    date: new Date("2022-05-1"),
+  },
 ];
 
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
@@ -50,7 +74,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
   },
 });
