3bb03b0f2819a4d5eb6a6e3fe61942775ab67b86
Timilehin Arigbede <Harriemannie@gmail.com>
2022-06-14 19:01:58 +0100

    Using Contextfrom Inside Components

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,7 +1,21 @@
+import { useContext } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../util/date";
 
 const RecentExpenses = () => {
-  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
+  const expensesCtx = useContext(ExpensesContext);
+
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const days7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date > days7DaysAgo;
+  });
+
+  return (
+    <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 Days" />
+  );
 };
 
 export default RecentExpenses;
